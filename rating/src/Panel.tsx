28089a98324e168becde2103430a0e9c1077aee3
aidar_syrdybaev <ajdarsyrdybaev@gmail.com>
2023-12-15 19:18:05 +0600

    added color rate

--- rating/src/Panel.tsx
+++ rating/src/Panel.tsx
@@ -8,11 +8,11 @@ import isThursday from 'date-fns/isThursday'
 import isFriday from 'date-fns/isFriday'
 import isSaturday from 'date-fns/isSaturday'
 import isSunday from 'date-fns/isSunday'
-import { ContributionDate } from './contributionDate';
+import { ContributionDate } from './ContributionDate';
 import "./Panel.css";
 
 const weekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
-const mounths = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль","Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+const mounths = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
 let result = await fetch("https://dpg.gg/test/calendar.json");
 let json = await result.json();
@@ -21,28 +21,28 @@ console.log(json);
 let currentDate = new Date();
 let firstDate = addDays(currentDate, -357);
 let firstDateOfWeek = startOfWeek(firstDate, { weekStartsOn: 1 });
-let datesByWeekDays: Array<Array<Date>> = [[],[],[],[],[],[],[]];
+let datesByWeekDays: Array<Array<Date>> = [[], [], [], [], [], [], []];
 
 [...Array(357).keys()].forEach(element => {
     let iterationDate = addDays(firstDateOfWeek, element);
-    if(isMonday(iterationDate)){
+    if (isMonday(iterationDate)) {
         datesByWeekDays[0].push(iterationDate);
-    } else if(isTuesday(iterationDate)){
+    } else if (isTuesday(iterationDate)) {
         datesByWeekDays[1].push(iterationDate);
     }
-    else if(isWednesday(iterationDate)){
+    else if (isWednesday(iterationDate)) {
         datesByWeekDays[2].push(iterationDate);
     }
-    else if(isThursday(iterationDate)){
+    else if (isThursday(iterationDate)) {
         datesByWeekDays[3].push(iterationDate);
     }
-    else if(isFriday(iterationDate)){
+    else if (isFriday(iterationDate)) {
         datesByWeekDays[4].push(iterationDate);
     }
-    else if(isSaturday(iterationDate)){
+    else if (isSaturday(iterationDate)) {
         datesByWeekDays[5].push(iterationDate);
     }
-    else if(isSunday(iterationDate)){
+    else if (isSunday(iterationDate)) {
         datesByWeekDays[6].push(iterationDate);
     }
 });
@@ -50,19 +50,57 @@ let datesByWeekDays: Array<Array<Date>> = [[],[],[],[],[],[],[]];
 const convertDateByComponent = (dates: Date[]) => {
     return dates.map(date => {
         const contributionCount = json[format(date, "yyyy-MM-dd")] ?? 0;
-        return <ContributionDate contributionCount={contributionCount} date={date}/>
+        return <ContributionDate contributionCount={contributionCount} date={date} />
     })
 }
 
 export const Panel = () => {
-    
-    return <div className='panel-container'>
-        {convertDateByComponent(datesByWeekDays[0])}
-        {convertDateByComponent(datesByWeekDays[1])}
-        {convertDateByComponent(datesByWeekDays[2])}
-        {convertDateByComponent(datesByWeekDays[3])}
-        {convertDateByComponent(datesByWeekDays[4])}
-        {convertDateByComponent(datesByWeekDays[5])}
-        {convertDateByComponent(datesByWeekDays[6])}
+
+    return <div className='container'>
+        <div className="contribution-container">
+            <div className='week-container'>
+                {weekDays.map(it => <span>{it}</span>)}
+            </div>
+            <div className='panel-container'>
+                {convertDateByComponent(datesByWeekDays[0])}
+                {convertDateByComponent(datesByWeekDays[1])}
+                {convertDateByComponent(datesByWeekDays[2])}
+                {convertDateByComponent(datesByWeekDays[3])}
+                {convertDateByComponent(datesByWeekDays[4])}
+                {convertDateByComponent(datesByWeekDays[5])}
+                {convertDateByComponent(datesByWeekDays[6])}
+            </div>
+        </div>
+        <div className="contribution-rate-container">
+            <span>меньше</span>
+            <div className='contribution-rate'>
+                <span className={`tooltip zero-count`}>
+                    <span className='tooltiptext'>
+                        No contributions
+                    </span>
+                </span>
+                <span className={`tooltip one-bigger-count`}>
+                    <span className='tooltiptext'>
+                        1-9 contributions
+                    </span>
+                </span>
+                <span className={`tooltip ten-bigger-count`}>
+                    <span className='tooltiptext'>
+                        10-19 contributions
+                    </span>
+                </span>
+                <span className={`tooltip twenty-bigger-count`}>
+                    <span className='tooltiptext'>
+                        20-29 contributions
+                    </span>
+                </span>
+                <span className={`tooltip thirty-bigger-count`}>
+                    <span className='tooltiptext'>
+                        30+ contributions
+                    </span>
+                </span>
+            </div>
+            <span>больше</span>
+        </div>
     </div>
 };
